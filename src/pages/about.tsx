8537a8af94316e467cf43411ac36f2a60b8b3eeb
AstralStoat17 <232106207+AstralStoat17@users.noreply.github.com>
2026-03-01 19:31:43 +0100

    Rename the project

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,22 +4,16 @@ const AboutPage = () => {
       <h2 class="text-2xl p-4">About</h2>
       <p class="max-w-xl">
         This is a remake of{" "}
-        <a href="https://roundest.t3.gg">a dumb app I made in Next.js</a>. This
+        <a href="https://roundest-t3.vercel.app/">a dumb app I made in Next.js</a>. This
         time it's in SolidJS!
       </p>
       <div class="p-4" />
       <ul>
         <li>
           {"- "}
-          <a class="text-blue-200 underline" href="https://twitter.com/t3dotgg">
-            Follow me on Twitter
-          </a>
-        </li>
-        <li>
-          {"- "}
           <a
             class="text-blue-200 underline"
-            href="https://github.com/TheoBr/democratic-geodude"
+            href="https://github.com/AstralStoat17/PokeSort"
           >
             Public Github repo
           </a>
